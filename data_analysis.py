3617f7b0e29b7e600f7c4c4ce85652fe9cc1257d
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-28 17:13:46 +0330

    Improve code readability by adding docstrings.

--- data_analysis.py
+++ data_analysis.py
@@ -1,13 +1,26 @@
 import pandas as pd
 
 class DataAnalyzer:
+    """
+    A class to analyze data using pandas.
+    """
+
     def __init__(self, data):
+        """
+        Initializes the DataAnalyzer with the provided data.
+        """
         self.data = data
 
     @classmethod
     def from_csv(cls, file_path):
+        """
+        Creates an instance of DataAnalyzer from a CSV file.
+        """
         data = pd.read_csv(file_path)
         return cls(data)
 
     def analyze(self):
+        """
+        Returns a statistical summary of the data.
+        """
         return self.data.describe()
