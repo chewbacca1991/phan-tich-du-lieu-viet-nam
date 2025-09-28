205ffed0741235581813aa31e5b3f95fce226951
chewbacca1991 <mounafbanbo@hotmail.com>
2025-09-28 17:13:31 +0330

    Add method to load data from a CSV file

--- data_analysis.py
+++ data_analysis.py
@@ -4,5 +4,10 @@ class DataAnalyzer:
     def __init__(self, data):
         self.data = data
 
+    @classmethod
+    def from_csv(cls, file_path):
+        data = pd.read_csv(file_path)
+        return cls(data)
+
     def analyze(self):
         return self.data.describe()
